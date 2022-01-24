848531e0dd345c2fbecaa598a8e9179fe0e328b3
nanzhang1991 <nanzhang1991@gmail.com>
2022-01-24 23:54:49 +0800

    update

--- app/common/dataload.py
+++ app/common/dataload.py
@@ -7,6 +7,19 @@ def load_json(json_path):
         dic = json.load(f)
     return dic
 
+  class Dataload:
+    @staticmethod   
+    def load_json(fp):
+        with open(fp, 'r', encoding='utf-8') as f:
+            dic = json.load(f)
+        return dic
+
+    @staticmethod
+    def to_json(dic, fp):
+        with open(fp, 'w') as f:
+            json.dump(dic, f, indent=2)  
+
+
 def convert_path(path):
     real_path = os.path.join(os.getcwd(), path)
     real_path = Path(real_path).as_posix()
