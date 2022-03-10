9532a105a94cb27bb872d54bb96061504fae745e
ZhangNan <47030616+NanZhang1991@users.noreply.github.com>
2022-03-10 21:01:14 +0800

    Update time_rotating_log.py

--- app/common/time_rotating_log.py
+++ app/common/time_rotating_log.py
@@ -1,37 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 27 13:11:24 2020
-@author: YJ001
-"""
-
+import os
 import logging
 from logging import handlers 
 
-def logger(filename, module_name):
+def get_logger(fn='log/'+ str(os.getppid()) + '.log', module_name="main", level=logging.INFO, when="midnight", interval=1, backupCount=31):
     logging.basicConfig()
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)    
-    th = handlers.TimedRotatingFileHandler(filename=filename, when="MIDNIGHT", interval=1, backupCount=3, encoding='utf-8')
+    th = handlers.TimedRotatingFileHandler(filename=fn, when=when, interval=interval, backupCount=backupCount, encoding='utf-8')
     th.suffix = "%Y-%m-%d.log"
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     th.setFormatter(formatter)
-    th.setLevel(logging.DEBUG)
+    th.setLevel(level)
     logger.addHandler(th)
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(level)
     logger.addHandler(stream_handler)
     logger.removeHandler(stream_handler)
     return logger
 
+logger = get_logger()
 
 if __name__ == "__main__":
-    logger = logger('app/log_output/my.log', __name__)
-    
-
     logger.debug('Quick zephyrs blow, vexing daft Jim.')
     logger.info('How quickly daft jumping zebras vex.')
     logger.error('unknow')
-    import os 
-    import sys
-    print(os.path.basename(sys.argv[0]))
+
