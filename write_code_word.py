25249e7abf0055164841415ad5f5b965a95fa5e3
nanzhang1991 <nanzhang1991@gmail.com>
2022-03-18 01:25:48 +0800

    update

--- write_code_word.py
+++ write_code_word.py
@@ -8,7 +8,7 @@ def getfilepath_write_word():
     file = open('allcode.docx','a+',encoding='utf-8')
     for parent,dirnames,filenames in os.walk(os.getcwd()):
         for filename in filenames:
-            path = Path(os.path.join(parent, filename)).as_posix()
+            path = Path(os.path.join(parent, filename)).as_posix() 
             if path.endswith('.py'):
                 with open(path,'r',encoding='utf-8') as f:
                     for line in f.readlines():
