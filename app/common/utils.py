2248c0a744f3d1e189437a8a04f8098daafba04d
ZhangNan <47030616+NanZhang1991@users.noreply.github.com>
2022-03-22 16:04:39 +0800

    Update utils.py

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,11 +1,28 @@
-def timer(message,log):
+"""基本组件"""
+
+
+import time
+
+
+def timer(file=__file__, logger=None):
+    """time-consuming computation decorator"""
     def wrapper(func):
         def insert_message(*args, **kwargs):
             t1= time.time()
             result = func(*args, **kwargs)
             t2 =time.time()
             cost_time = t2-t1
-            log.info(f"函数{func.__name__}， {message} 花费时间：{cost_time}秒")
+            if logger:
+                logger.info(f" module:{file} \n function:{func.__name__}, time consuming:{cost_time}")
+            else:
+                print(f"module:{file} \n function:{func.__name__}, time consuming:{cost_time}秒")
             return result
         return insert_message
     return wrapper
+
+if __name__=="__main__":
+    @timer()
+    def test():
+        return 1
+
+    print(test())
