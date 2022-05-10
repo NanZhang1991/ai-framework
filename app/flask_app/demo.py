a8f563c4bf33bbcaa17b2da638a8ab46d49358de
ZhangNan <47030616+NanZhang1991@users.noreply.github.com>
2022-05-10 14:16:05 +0800

    Update demo.py

--- app/flask_app/demo.py
+++ app/flask_app/demo.py
@@ -13,33 +13,37 @@ def main():
     # json
     data = request.get_json()
     service_name = data.get('service_name')
+    # default code 302 return get request， code 307 return post request
     print(f"{'*'*10} {redirect(url_for(service_name), code=307)} {'*'*10}")
     return redirect(url_for(service_name), code=307)
 
 
 @app.route('/v1/co/<service_name>', methods=['POST'], strict_slashes=False)
 def main_0(service_name):
-    # 字符串/json
+    # text/json
     data = request.get_data()
     data = json.loads(data)
     data = data.get('service_name')
     print(f"{'*'*10} {data} {'*'*10}")
-    # 默认code 302 get请求， code 307 返回post 请求
-    return redirect(url_for(service_name), code=307)
+    return redirect(url_for(service_name))
 
 
 @app.route('/v1/co/service', methods=['POST'], strict_slashes=False)
 def main_1():
-    # 表单
+    # form-data
     data = request.form.get('data')
     print(f"{'*'*10} {data} {'*'*10}")
     return make_response(jsonify(data), 404)
 
 
-@app.route('/v1/co/service_test', methods=['POST'], strict_slashes=False)
+@app.route('/v1/co/service_test',
+           methods=['GET', 'POST'], strict_slashes=False)
 def service_demo():
-    return make_response(
-        jsonify('service_demo post port test was successful!'), 200)
+    if request.method == 'GET':
+        result = jsonify('service_demo get port test was successful!')
+    else:
+        result = jsonify('service_demo post port test was successful!')
+    return make_response(result, 200)
 
 
 if __name__ == '__main__':
