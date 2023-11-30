e1650f03598fe9e43da54bd88346fe9cc723e552
ZhangNan <47030616+NanZhang1991@users.noreply.github.com>
2023-11-30 18:47:42 +0800

    Update utils.py

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,22 +1,35 @@
 """基本组件"""
 import time
+from functools import wraps
 
-
-def timer(file=__file__, logger=None):
+def timer(module='s',logger=None):
     """time-consuming computation decorator"""
     def wrapper(func):
         def insert_message(*args, **kwargs):
-            t1 = time.time()
+            start_time = time.perf_counter()
             result = func(*args, **kwargs)
-            t2 = time.time()
-            cost_time = t2-t1
+            cost_time = time.perf_counter() - start_time
             if logger:
-                logger.info("module:{} \n function:{}, time consuming:{}s".format(file, func.__name__, cost_time))
+                logger.info("module:%s \n function:%s, time consuming:%ss",
+                            file, func.__name__, cost_time)
             else:
-                print("module:{} \n function:{}, time consuming:{:.5f}s".format(file, func.__name__, cost_time))
+                print(f"module:{module} \n function:{func.__name__},time consuming:{cost_time:.5f}s")
             return result
         return insert_message
     return wrapper
+
+
+def time_master(func):
+    @wraps(func)
+    def call_func(*args, **kwargs):
+        start_time = time.time()
+        res = func(*args, **kwargs)
+        exec_time = f"{time.time() - start_time :.2f}s"
+        print(f'{func.__name__} function exec time {exec_time}')
+        # logger.info('%s function exec time %s', func.__name__, exec_time)
+        return res
+    return call_func
+
 
 if __name__=="__main__":
     @timer()
